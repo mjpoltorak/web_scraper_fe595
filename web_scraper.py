c8be0ec0f61728b00859994c1bb6980e6c23b53f
Matthew Poltorak <Matthew.Poltorak@verizon.net>
2020-10-06 22:37:17 -0400

    Commit assignment

---
+++ web_scraper.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+
+
+def web_scraper():
+    df = pd.DataFrame(columns=['Name', 'Purpose'])
+    for i in range(50):
+        res = requests.get('http://3.95.249.159:8000/random_company').text
+        split = res.split('<ol>')[1].split('</ol>')[0].split('<li>')
+        d = {}
+        for s in split:
+            info = s.replace('\n', '').replace('</li>', '').strip(' ')
+            if 'Name:' in info:
+                d['Name'] = info.split('Name: ')[1]
+            if 'Purpose:' in info:
+                d['Purpose'] = info.split('Purpose: ')[1]
+        df = df.append(d, ignore_index=True)
+    df.to_csv('companies.csv')
+    print()
+
+
+if __name__ == '__main__':
+    web_scraper()
